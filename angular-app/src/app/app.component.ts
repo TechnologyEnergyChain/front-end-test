c18a2c82e826f9949da51824175217121f123ee6
Marcos <devler.mve@gmail.com>
2024-05-29 13:08:33 +0200

    feat: app.component.ts has been updated to start the game

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,18 +1,35 @@
-import {Component, HostListener, inject, Inject, OnDestroy, OnInit} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {GamePLoC} from "../../../core/game/presentation/GamePLoC";
-import {DependencyLocator} from "../lib/dependency-injection/DependencyLocator";
-import {TileComponent} from "../lib/ui/atoms/tile/tile.component";
-import {BoardComponent} from "../lib/ui/molecules/board/board.component";
-import {UseGameStore} from "../lib/store/UseGameStore";
-import {SetupKeyboard} from "../lib/helpers/SetupKeyboard";
-import {UseGuessStore} from "../lib/store/UseGuessStore";
+import {TileComponent} from "@lib/ui/atoms/tile/tile.component";
+import {BoardComponent} from "@lib/ui/molecules/board/board.component";
+import {UseGameStore} from "@lib/store/UseGameStore";
+import {SetupKeyboard} from "@lib/helpers/SetupKeyboard";
+import {UseGuessStore} from "@lib/store/UseGuessStore";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: '',
+  imports: [RouterOutlet, TileComponent, BoardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  providers: [UseGameStore, UseGuessStore]
 })
-export class AppComponent {
+export class AppComponent extends SetupKeyboard implements OnInit, OnDestroy {
+  constructor() {
+    super(
+      inject(UseGuessStore),
+      inject(UseGameStore)
+    );
+  }
+
+  async ngOnInit() {
+    await this.gameStore.ploc.start()
+    await this.gameStore.ploc.getGame()
+  }
+
+  ngOnDestroy(): void {
+    this.gameStore.unsubscribe()
+  }
+
 
 }
